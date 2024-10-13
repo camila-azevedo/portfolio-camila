f2a6eca63bef23b3ec35d5b93ed3e638b03ea302
Camila Azevedo <camila.azevedo@sempreceub.com>
2024-10-13 13:40:38 -0300

    Create maioridade.java

---
+++ maioridade.java
@@ -0,0 +1,9 @@
+public class Maioridade {
+    public static void main(String[] args) {
+
+  // Declaração de Variáveis
+      Scanner leitor = new Scanner(System.in);
+      int idade = 0
+
+    }
+}
